3c78930fcce29adb7de83ab6d8bef5b0a018dab2
samikhailov <31734097+samikhailov@users.noreply.github.com>
2020-03-03 20:39:05 +0300

    Update grazhdanskaia-oborona.py

--- week_6/grazhdanskaia-oborona.py
+++ week_6/grazhdanskaia-oborona.py
@@ -1,22 +1,20 @@
-if __name__ == "__main__":
-    amount_towns = int(input())
-    towns = list(enumerate(map(int, input().split()[:amount_towns])))
-    amount_shelters = int(input())
-    shelters = list(enumerate(map(int, input().split()[:amount_shelters])))
-    towns.sort(key=lambda k: k[1])
-    shelters.sort(key=lambda k: k[1])
+amount_towns = int(input())
+towns = list(enumerate(map(int, input().split()[:amount_towns]), 1))
+amount_shelters = int(input())
+shelters = list(enumerate(map(int, input().split()[:amount_shelters]), 1))
+towns.sort(key=lambda k: k[1])
+shelters.sort(key=lambda k: k[1])
 
-    index = 0
-    result = []
+index = 0
+result = []
 
-    for i in range(amount_towns):
-        while index != amount_towns - 1 and abs(
-            towns[i][1] - shelters[index][1]
-            ) > abs(
-            towns[i][1] - shelters[index + 1][1]
-        ):
-            index += 1
-        else:
-            result.append([shelters[index][0] + 1, towns[i][0]])
-    result.sort(key=lambda p: p[1])
-    print(*[i[0] for i in result])
+for town in towns:
+    while (index + 1 < amount_shelters and abs(
+            town[1] - shelters[index][1]) > abs(
+            town[1] - shelters[index + 1][1])):
+        index += 1
+    else:
+        result.append([town[0], shelters[index][0]])
+
+
+print(*[i[1] for i in sorted(result, key=lambda k: k[0])])
